88bc049eeb9562e3810d0342d80e449f21416d14
Mardawi <ola.mardawi@gmail.com>
2022-03-02 18:43:27 +0200

    oula's changes
    I have preserved and restored the state of the application either the switch is On which indicates the light modes otherwise indicates the Off mode

--- AutoLayout/ViewController.swift
+++ AutoLayout/ViewController.swift
@@ -9,30 +9,40 @@ import UIKit
 
 class ViewController: UIViewController {
 
-  
-    
-    
-  
     @IBOutlet var mySwitch: UISwitch!
     
+    let defaults = UserDefaults.standard
+    var state: Bool?
+
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
-    
-    @IBAction func switchDidChange(_ sender: UISwitch){
-        if sender.isOn{
-            //view.backgroundColor = .black
-            view.window?.overrideUserInterfaceStyle = .dark
-
-        }
-          
-        else{
-          //  view.backgroundColor = .white
-            view.window?.overrideUserInterfaceStyle = .light
-        }
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        let savedState = defaults.bool(forKey: "DarkModeOption")
+        view.window?.overrideUserInterfaceStyle = savedState ? .light : .dark
+        mySwitch.setOn(savedState, animated: true)
     }
     
+    
+    @IBAction func switchDidChange(_ sender: UISwitch){
+
+        if sender.isOn {
+            //view.backgroundColor = .black
+            state = true
+            view.window?.overrideUserInterfaceStyle = .light
+        }else {
+          //  view.backgroundColor = .white
+            state = false
+            view.window?.overrideUserInterfaceStyle = .dark
+        }
+        
+        defaults.set(state, forKey: "DarkModeOption")
+        
+    }
+    
+   
 
 
 }
