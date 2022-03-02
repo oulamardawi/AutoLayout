41d0f110aa984a7b4fa5233d2ef59fd3308e22ce
Mardawi <ola.mardawi@gmail.com>
2022-03-02 16:10:30 +0200

    oula's changes
    I enabled the dark mode by pressing on the switch, it is going to operate in dark mode if the switch is on otherwise it is going to operate in Light mode.

--- AutoLayout/ViewController.swift
+++ AutoLayout/ViewController.swift
@@ -9,10 +9,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+  
+    
+    
+  
+    @IBOutlet var mySwitch: UISwitch!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
+    
+    
+    @IBAction func switchDidChange(_ sender: UISwitch){
+        if sender.isOn{
+            //view.backgroundColor = .black
+            view.window?.overrideUserInterfaceStyle = .dark
+
+        }
+          
+        else{
+          //  view.backgroundColor = .white
+            view.window?.overrideUserInterfaceStyle = .light
+        }
+    }
+    
 
 
 }
